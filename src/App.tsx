70c31a074e546d6f7ef49f13727547c010fe4f33
MarcosPaulo62 <marcospaulosc03@gmail.com>
2023-08-10 11:29:03 -0300

    Header pronto

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import "./App.css";
 import { CardProduct } from "./components/CardProduct";
 import { Product } from "./models/product";
 import { v4 as uuidv4 } from "uuid";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const product: Product = {
   id: uuidv4(),
@@ -15,7 +18,21 @@ const product: Product = {
 function App() {
   return (
     <>
-      <CardProduct product={product} />
+      <div className="body">
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            {/* <Route path='/' element={<Home />} /> */}
+            {/* <Route path='/*' element={<NotFound />} /> */}
+          </Routes>
+        </BrowserRouter>
+
+        <CardProduct product={product} />
+        <CardProduct product={product} />
+        
+        <Footer />
+      </div>
+      
     </>
   );
 }
