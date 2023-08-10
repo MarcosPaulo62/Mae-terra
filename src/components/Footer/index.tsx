365d1348bfe8db6abe0dd9a4551adfc94413c731
MarcosPaulo62 <marcospaulosc03@gmail.com>
2023-08-10 13:59:58 -0300

    Footer pronto

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,12 +1,19 @@
-
+import { useState } from 'react';
+import ModalFaleConosco from '../ModalFaleConosco';
+import './index.css';
 
 export default function Footer(){
+    const [modal, setModal] = useState<boolean>(false);
+
+    const showModal = () => setModal(!modal);
+
     return(
         <footer>
             <p className="label">Copyright &copy; 2023</p>
-            <button type="button">
+            <button type="button" onClick={showModal}>
                 Fale consoco
             </button>
+            {modal && <ModalFaleConosco active={setModal} />}
         </footer>
     )
 }
