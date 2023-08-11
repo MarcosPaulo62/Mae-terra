29dbda558844b03598258f591778c9e261d4bc6f
Fabiola <fabiolars2009@hotmail.com>
2023-08-11 02:04:34 -0300

    user finnaly

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home/home";
 import Category from "./pages/Category/Category";
 import Cart from "./pages/Cart";
-import User from './pages/User/User';
-
-
+import User from "./pages/User/User";
 
 function App() {
   return (
@@ -16,16 +14,14 @@ function App() {
         <BrowserRouter>
           <Header />
           <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/carrinho' element={<Cart />} />
-            <Route path='/category/:tag' element={<Category />} /> 
-            <Route path='/user' element={<User />} /> 
-            {/* <Route path='/*' element={<NotFound />} /> */}                                                                                                                   />} /> */}
+            <Route path="/" element={<Home />} />
+            <Route path="/carrinho" element={<Cart />} />
+            <Route path="/category/:tag" element={<Category />} />
+            <Route path="/user" element={<User />} />
           </Routes>
         </BrowserRouter>
         <Footer />
       </div>
-      
     </>
   );
 }
--- src/pages/User/User.tsx
+++ src/pages/User/User.tsx
@@ -1,25 +1,116 @@
-import { Box, Typography, Button } from '@mui/material';
+import { Typography, Button, TextField } from "@mui/material";
+import { useState } from "react";
+import "./style.css";
+import { Box } from "@mui/system";
+import { makeStyles } from "@mui/styles";
+const tabs = ["Cadastro", "Pedidos", "Desejos"];
+
+const useStyles = makeStyles(() => ({
+  containedButton: {
+    backgroundColor: "#355d4c",
+    "&:hover": {
+      backgroundColor: "#1e342b",
+    },
+  },
+  textButton: {
+    color: "#355d4c",
+  },
+}));
 
 export default function User() {
+  const [activeTab, setActiveTab] = useState("Cadastro");
+  const [changeData, setChangeData] = useState("Alterar dados");
+  const [state, setState] = useState(true);
+  const classes = useStyles();
 
+  const readonly = () => {
+    if (state) {
+      setChangeData("Salvar dados");
+      setState(false);
+    } else {
+      setChangeData("Alterar dados");
+      setState(true);
+    }
+  };
+  return (
+    <>
+      <main className="user">
+        <div className="user-content">
+          <div className="header-user">
+            <h1 className="title-user">Minha conta</h1>
+            <Typography>
+              Seja bem vindo (a), <span className="bolder">Ana Banana</span>
+            </Typography>
+          </div>
+          <div className="nav-user">
+            {tabs.map((tab) => (
+              <Button
+                key={tab}
+                variant={tab === activeTab ? "contained" : "text"}
+                onClick={() => setActiveTab(tab)}
+                className={
+                  tab === activeTab
+                    ? classes.containedButton
+                    : classes.textButton
+                }
+              >
+                {tab}
+              </Button>
+            ))}
+          </div>
+          <div className="form-user">
+            <h2 className="title-form">Dados pessoais</h2>
+            <Box>
+              <Typography>Login</Typography>
+              <TextField
+                id="outlined-basic"
+                variant="outlined"
+                defaultValue="anamorango@gmail.com"
+                size="small"
+                disabled={state}
+              />
+            </Box>
 
-    return(
-        <>
-            <main className="user">
-                <div className="header-user">
-                    <h1 className="title-user">Minha conta</h1>
-                    <Typography>Seja bem vindo (a), <span className="bolder">Ana Banana</span>
-                    </Typography>
-                </div>
-                <div className="nav-user">
-                    <Button variant="contained">Cadastro</Button>
-                    <Button variant="text">Pedidos</Button>
-                    <Button variant="text">Desejos</Button>
-                </div>
-                <div className="form-user">
+            <Box>
+              <Typography>Nome de usuário</Typography>
+              <TextField
+                id="outlined-basic"
+                variant="outlined"
+                disabled={state}
+                defaultValue="Ana Banana"
+                size="small"
+              />
+            </Box>
 
-                </div>
-            </main>
-        </>
-    )
+            <Box>
+              <Typography>Telefone</Typography>
+              <TextField
+                id="outlined-basic"
+                variant="outlined"
+                disabled={state}
+                defaultValue="(11) 7070-7070"
+                size="small"
+              />
+            </Box>
+
+            <Box>
+              <Typography>Senha</Typography>
+              <TextField
+                id="outlined-basic"
+                variant="outlined"
+                type="password"
+                defaultValue="1234"
+                size="small"
+                disabled={state}
+              />
+            </Box>
+
+            <button className="btn btn-add" onClick={() => readonly()}>
+              {changeData}
+            </button>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
