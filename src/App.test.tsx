a5dbc0f5bea0d22724974559273310b08aaa05a1
MarcosPaulo62 <marcospaulosc03@gmail.com>
2023-08-14 22:36:12 -0300

    wip

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,17 +2,16 @@ import { describe, expect, it } from "vitest"
 
 import App from "./App";
 
-import { render, screen, userEvent } from "../test-utils";
+import { render, screen, userEvent } from "@testing-library/react";
 
 
 
 describe("Simple working test", () => {
 
-  it("fake test", () => {
+  it("test", () => {
 
     render(<App />);
 
     expect("ola").toBe("ola");
-
   });
 });
